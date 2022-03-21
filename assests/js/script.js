50a6f242c5a3ebc96ad83255639c0992980abd38
chris chubb <chubb91chris@icloud.com>
2022-03-20 21:13:08 -0400

    bootstrap -css added functions
    cant get objects to show on screen

---
+++ assests/js/script.js
@@ -0,0 +1,21 @@
+
+var fetchButton= document.querySelector(".fetchApi")
+var recipesDataDisplay= document.querySelector(".recipesDisplay")
+
+function getapi (){
+    var requestUrl= "https://api.edamam.com/api/recipes/v2?type=public&q=Data&app_id=4cd7c7b2&app_key=30a6424795a6d890b5099d0e7e386fab";
+    
+    fetch(requestUrl)
+    .then(function(response){
+        console.log(response)
+        return response.json();
+        
+    })
+
+ 
+
+  
+        
+
+}
+fetchButton.addEventListener("click",getapi())
