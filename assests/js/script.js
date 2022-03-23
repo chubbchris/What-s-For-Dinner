981547d7902b93c554fbafa611197ce80177839a
chris chubb <chubb91chris@icloud.com>
2022-03-22 22:17:48 -0400

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -2,7 +2,7 @@
 var fetchButton= document.querySelector(".fetchApi")
 var recipesDataDisplay= document.querySelector(".recipesDisplay")
 
-function getapi (){
+```function getapi (){
     var requestUrl= "https://api.edamam.com/api/recipes/v2?type=public&q=Data&app_id=4cd7c7b2&app_key=30a6424795a6d890b5099d0e7e386fab";
     
     fetch(requestUrl)
@@ -11,7 +11,9 @@ function getapi (){
         return response.json();
         
     })
-
+ .then(function(data){
+     console.log(data)
+ })
  
 
   
