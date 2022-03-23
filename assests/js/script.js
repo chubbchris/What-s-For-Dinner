2429eb44b4639a31d56f3d53d48f9d38d74092e2
chris chubb <chubb91chris@icloud.com>
2022-03-23 19:39:31 -0400

    added- drink api and drink button deleted nav

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,9 @@
 
-var fetchButton= document.querySelector(".fetchApi")
-var recipesDataDisplay= document.querySelector(".recipesDisplay")
+var fetchFoodButton= document.querySelector(".fetchFoodApi");
+var fetchBeerButton=document.querySelector(".fetchBeerApi");
+var recipesDataDisplay= document.querySelector(".recipesDisplay");
 
-function getapi (){
+function getFoodApi (){
     var requestUrl= "https://api.edamam.com/api/recipes/v2?type=public&q=Data&app_id=4cd7c7b2&app_key=30a6424795a6d890b5099d0e7e386fab";
     
     fetch(requestUrl)
@@ -14,10 +15,19 @@ function getapi (){
  .then(function(data){
      console.log(data)
  })
- 
-
-  
-        
-
 }
-fetchButton.addEventListener("click",getapi())
+ function getBeersApi (){
+    var requestUrl= "https://api.punkapi.com/v2/beers";
+    
+    fetch(requestUrl)
+    .then(function(response){
+        console.log(response)
+        return response.json();
+        
+    })
+ .then(function(data){
+     console.log(data)
+ })
+}
+fetchFoodButton.addEventListener("click",getFoodApi());
+fetchBeerButton.addEventListener("click",getBeersApi());
