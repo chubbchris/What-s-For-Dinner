5840c454d45bb2767a29f20ee8b2d695eabb5af2
chris chubb <chubb91chris@icloud.com>
2022-03-24 17:20:24 -0400

    objects are showing

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,9 +5,6 @@ var recipesDataDisplay= document.querySelector(".recipesDisplay");
 var selectCuisineInput= document.querySelector("#selectCuisineStyle")
 var selectDietInput= document.querySelector("#selectDietLabel")
 
-console.log(selectCuisineInput)
-console.log(selectDietInput)
-
 var getFoodApi= function  (){
     var requestUrl= "https://api.edamam.com/api/recipes/v2?type=public&q=Data&app_id=4cd7c7b2&app_key=30a6424795a6d890b5099d0e7e386fab";
     
@@ -18,20 +15,18 @@ var getFoodApi= function  (){
         
     })
  .then(function(data){
-    console.log(data.hits[i]);
-     for(var i=0;i<data.hits.length;i++)
-     var recipeDiv = document.getElementById("recipe");
-    var recipeDivEl = document.createElement("li");
-    recipeDivEl.textContent = data;
-    recipeDiv.appendChild(recipeDivEl);
-    console.log(data.hits[i]);
+    
+ for (var i=0;i<data.hits.length;i++){
+    var recipeDiv= document.querySelector("#recipe")
+    var recipeDivEl= document.createElement("li")
+    recipeDivEl.textContent= data.hits[i]
+    recipeDiv.appendChild(recipeDivEl)
+     console.log(data.hits[i])
 
-    
-   
-    
+ }
     
  })
-}
+};
 var getBeersApi =  function (){
     var requestUrl= "https://api.punkapi.com/v2/beers";
     
@@ -42,7 +37,17 @@ var getBeersApi =  function (){
         
     })
  .then(function(data){
-     console.log(data);
+    var numbers= Math.floor(Math.random() * 24)
+    for (var i=0;i<data.length;i++){
+        var beerDiv= document.querySelector("#beer");
+        var beerDivEl=document.createElement("li");
+        beerDivEl.textContent=data[i]
+        beerDiv.appendChild(beerDivEl);
+    
+     console.log(data[i]);
+     console.log(numbers)
+    
+    }
  });
 
 
